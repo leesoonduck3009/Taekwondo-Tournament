34f39e0d0e0ad29a8aaef241a9d40f7f5c5a5c43
PNB309 <nguyen.phucbinh445@gmail.com>
2025-03-18 23:54:17 +0700

    feat(Tournament): Add upload excel to tournamenet

--- src/routes/v1/tournamentGroup.route.ts
+++ src/routes/v1/tournamentGroup.route.ts
@@ -1,8 +1,18 @@
-import { Router } from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import { v1Controller } from "../../controllers/v1";
+import multer from "multer";
 const route = Router();
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 route.get("/", v1Controller.TournamentGroup.getAll);
 route.post("/", v1Controller.TournamentGroup.create);
+route.post(
+  "/upload",
+  upload.single("file"),
+  async (req: Request, res: Response, next: NextFunction) => {
+    await v1Controller.TournamentGroup.uploadTournamentsByExcel(req, res, next);
+  }
+);
 route.get("/:id", v1Controller.TournamentGroup.getById);
 route.put("/:id", v1Controller.TournamentGroup.updateById);
 route.delete("/:id", v1Controller.TournamentGroup.deleteById);
