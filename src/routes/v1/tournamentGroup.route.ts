f21c6bbaedf98bf97f00a7fc4904bc6071dbd439
PNB309 <nguyen.phucbinh445@gmail.com>
2025-03-18 20:55:39 +0700

    fix/fix tournamentGrou[ controller

--- src/routes/v1/tournamentGroup.route.ts
+++ src/routes/v1/tournamentGroup.route.ts
@@ -3,7 +3,6 @@ import { controllerV1 } from "../../controllers/v1";
 const route = Router();
 route.get("/", controllerV1.TournamentGroup.getAll);
 route.post("/", controllerV1.TournamentGroup.create);
-route.get("/helloworld", controllerV1.TournamentGroup.helloWorld);
 route.get("/:id", controllerV1.TournamentGroup.getById);
 route.put("/:id", controllerV1.TournamentGroup.updateById);
 route.delete("/:id", controllerV1.TournamentGroup.deleteById);
