f83e4dc3b283a97b882dece705212284aadd14c9
PNB309 <nguyen.phucbinh445@gmail.com>
2025-04-05 00:33:40 +0700

    fix

--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 const ENV = process.env.NODE_ENV || "development";
 // Load .env file
 dotenv.config({ path: `.${ENV}.env` });
-
+console.log(`CONNECTION_STRING: ${process.env.CONNECTION_STRING}`);
 export const config = {
   env: ENV,
   port: process.env.PORT || 3000,
